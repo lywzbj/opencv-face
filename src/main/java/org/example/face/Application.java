e3a55f4544c62615e4d7bfd0c0f66c8ce3446249
18715838915 <19715838915@163.com>
2023-08-31 17:49:49 +0800

    获取摄像头,采集数据

--- src/main/java/org/example/face/Application.java
+++ src/main/java/org/example/face/Application.java
@@ -2,17 +2,45 @@ package org.example.face;
 
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
 
 public class Application {
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        Mat mat = Mat.eye(3,3, CvType.CV_8UC1);
+        VideoCapture capture = new VideoCapture(0);
+        if (!capture.isOpened()) {
+            System.out.println("Camera not found.");
+            return;
+        }
+        // Create a Mat object to store each frame
+        Mat frame = new Mat();
+        int frameCount = 0;
+        while (true) {
+            // Capture a frame from the camera
+            capture.read(frame);
+            // Check if the frame is valid
+            if (!frame.empty()) {
+                // Save the frame as an image every 5 seconds
+                if (frameCount % 150 == 0) {  // 30 frames per second, so 150 frames ≈ 5 seconds
+                    String filename = "frame_" + frameCount + ".png";
+                    Imgcodecs.imwrite(filename, frame);
+                    System.out.println("Saved: " + filename);
+                }
+                frameCount++;
+            }
 
-        System.out.println("mat = " + mat.dump());
+            try {
+                Thread.sleep(33);  // Approximately 30 frames per second
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
 
 
     }
